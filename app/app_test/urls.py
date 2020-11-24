4542710363f5364dca65b251483d26805666c342
SpeciAlSerega <alexeykoko1987@mail.ru>
2020-11-24 14:58:27 +0000

    Update

--- app/app_test/urls.py
+++ app/app_test/urls.py
@@ -12,6 +12,20 @@ urlpatterns = [
     path('prices/', prices, name='prices'),
     path('spares/', spares, name='spares'),
 
+# ####################################################################################
+    path('posts/', posts_list, name='posts_list_url'),
+    path('tags/', tags_list, name='tags_list_url'),
+# ####################################################################################
+    path('post/create/', PostCreate.as_view(), name='post_create_url'),
+    path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
+    path('tag/create/', TagCreate.as_view(), name = 'tag_create_url'),
+    path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url'),
+# ####################################################################################
+    path('post/<str:slug>/update/', PostUpdate.as_view(), name='post_update'),
+    path('tag/<str:slug>/update/', TagUpdate.as_view(), name='tag_update'),
+# ####################################################################################
+    # path('post/<str:slug>/delete/', TagDelete.as_view(), name='post_delete_url'),  
+    # path('tag/<str:slug>/delete/', TagDelete.as_view(), name='tag_delete_url'),  
 
 
 ]
